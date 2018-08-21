d88ab9b771f783282bdbb71b75b4b2ed14fb6caf
WaitheraMbugua <waitheralmbugua@gmail.com>
2018-08-21 17:38:49 +0300

    UI feedback implementation

--- API/tests/test_Questions.py
+++ API/tests/test_Questions.py
@@ -1,80 +1,80 @@
-import pytest
-import json
+from unittest import TestCase
+from app import create_app
+import Questions.views
 import os
-import Questions.model
+import json
 
-
-class QuestionsTestCase(unittest.TestCase):
-    """This class represents the bucketlist test case"""
+class BaseTestCase(TestCase):
 
     def setUp(self):
-        """Define test variables and initialize app."""
-        self.app = create_app(config_name="testing")
-        self.client = self.app.test_client
-        self.questions = {'name': 'Go to Borabora for vacation'}
 
-        # binds the app to the current context
-        with self.app.app_context():
-            # create all tables
-            db.create_all()
+        """Configure test enviroment."""
+
+        os.environ['APP_SETTINGS'] = 'Testing'
+        self.app = create_app("Testing")
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        self.test_client = self.app.test_client()
+
+    def tearDown(self):
+        self.app_context.pop()
+
+
+
+class QuestionsTestCase(BaseTestCase):
 
     def test_question_creation(self):
-        """Test API can create a bucketlist (POST request)"""
-        res = self.client().post('/Questions/', data=self.bucketlist)
+        """Test API can create a question (POST request)"""
+        res = self.client().post('/Questions/', data=self.questions)
         self.assertEqual(res.status_code, 201)
-        self.assertIn('Go to Borabora', str(res.data))
+
 
     def test_api_can_get_all_questions(self):
-        """Test API can get a bucketlist (GET request)."""
+        """Test API can get a question (GET request)."""
         res = self.client().post('/Questions/', data=self.question)
         self.assertEqual(res.status_code, 201)
-        res = self.client().get('/bucketlists/')
+        res = self.client().get('/questions/')
         self.assertEqual(res.status_code, 200)
         self.assertIn('Go to Borabora', str(res.data))
 
     def test_api_can_get_questions_by_id(self):
-        """Test API can get a single bucketlist by using it's id."""
-        rv = self.client().post('/bucketlists/', data=self.bucketlist)
+        """Test API can get a single question by using it's id."""
+        rv = self.client().post('/questions/', data=self.question)
         self.assertEqual(rv.status_code, 201)
         result_in_json = json.loads(rv.data.decode('utf-8').replace("'", "\""))
         result = self.client().get(
             '/Questions/{}'.format(result_in_json['id']))
         self.assertEqual(result.status_code, 200)
-        self.assertIn('Go to Borabora', str(result.data))
+        self.assertIn('question', str(result.data))
 
     def test_questions_can_be_edited(self):
         """Test API can edit an existing question. (PUT request)"""
         rv = self.client().post(
             '/Questions/',
-            data={'name': 'Eat, pray and love'})
+            data={'01': 'How do you create a list in python?'})
         self.assertEqual(rv.status_code, 201)
         rv = self.client().put(
-            '/Questions/1',
+            '/Questions/01',
             data={
-                "name": "Dont just eat, but also pray and love :-)"
+                "01": "How do you create a list in python?"
             })
         self.assertEqual(rv.status_code, 200)
-        results = self.client().get('/Questions/1')
-        self.assertIn('Dont just eat', str(results.data))
+        results = self.client().get('/Questions/01')
+        self.assertIn('How do you create a string in python? ', str(results.data))
 
     def test_question_deletion(self):
-        """Test API can delete an existing bucketlist. (DELETE request)."""
+        """Test API can delete an existing question. (DELETE request)."""
         rv = self.client().post(
             '/Questions/',
-            data={'name': 'Eat, pray and love'})
+            data={'01': 'How do you create a list in python?'})
         self.assertEqual(rv.status_code, 201)
-        res = self.client().delete('/bucketlists/1')
+        res = self.client().delete('/questions/1')
         self.assertEqual(res.status_code, 200)
         # Test to see if it exists, should return a 404
         result = self.client().get('/Question/1')
         self.assertEqual(result.status_code, 404)
 
-    def tearDown(self):
-        """teardown all initialized variables."""
-        with self.app.app_context():
-            # drop all tables
-            db.session.remove()
-            db.drop_all()
+    
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
